6c5adaf69b89ad8ff9ad048a923ed2e010f32a4f
Oscar <munozmejiaoscarmauricio@gmail.com>
2026-04-27 22:00:34 -0500

    corrections config lint 3

--- eslint.config.js
+++ eslint.config.js
@@ -2,7 +2,6 @@ import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
-import react from 'eslint-plugin-react'
 
 export default [
   { ignores: ['dist'] },
@@ -20,7 +19,6 @@ export default [
     plugins: {
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
-      react,
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
